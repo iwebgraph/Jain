ec1aa4921109695a988e93a3f61ec8e9995876b5
nikhil344jain <50509379+nikhil344jain@users.noreply.github.com>
2019-09-08 20:18:02 +0530

    Update login.js

--- router/login.js
+++ router/login.js
@@ -6,16 +6,17 @@ const _ = require('lodash');
 //const config = require('../');
 
 router.post("/", function (req, res) {
-    let query ="SELECT * FROM `user` WHERE  email = '" + req.body.email +"' AND password='" +req.body.password +"'";
-    connection.query(query, function (error,result) {
-            if (result !=undefined && result.length > 0) {
-                const user = _.first(result);
-                const token = jwt.sign({username:user.username,password:user.password },"test");
-                res.send({'x-token':token});
-            } else {
-                res.send(null);
-            }
+    console.log('body', req.body)
+    let query = "SELECT * FROM `users` WHERE  email = '" + req.body.email + "' AND password='" + req.body.password + "'";
+    connection.query(query, function (error, result) {
+        if (result != undefined && result.length > 0) {
+            const user = _.first(result);
+            const token = jwt.sign({ username: user.username, password: user.password }, "test");
+            res.send({ 'x-token': token });
+        } else {
+            res.send(null);
         }
+    }
     );
 });
 module.exports = router;
